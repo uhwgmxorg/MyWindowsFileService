b3f4a3d8e2aaefc0f7a2615f0105e20b660b3a77
Uwe Walter <uhw@gmx.org>
2021-07-25 10:42:15 +0200

    Missing configuration.json.

--- Bat/InstallOnC.bat
+++ Bat/InstallOnC.bat
@@ -3,6 +3,7 @@ if not exist "C:\MyWindowsFileService\Import\" mkdir C:\MyWindowsFileService\Imp
 if not exist "C:\MyWindowsFileService\Import\Done\" mkdir C:\MyWindowsFileService\Import\Done
 if not exist "C:\MyWindowsFileService\Bat\" mkdir C:\MyWindowsFileService\Bat
 copy *.bat C:\MyWindowsFileService\Bat
+copy ..\MyWpfWindowsServiceControlApp\configuration.json C:\MyWindowsFileService\
 copy ..\MyWindowsFileService\bin\Debug\net5.0\ref\*.* C:\MyWindowsFileService\ref
 copy ..\MyWindowsFileService\bin\Debug\net5.0\MyWindowsFileService.exe C:\MyWindowsFileService
 copy ..\MyWindowsFileService\bin\Debug\net5.0\*.json C:\MyWindowsFileService
